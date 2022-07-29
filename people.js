c43848c7df190aba126fe56f951a8531335cdc4b
mokashi21 <101655816+mokashi21@users.noreply.github.com>
2022-07-29 15:25:10 +0530

    Add files via upload

---
+++ people.js
@@ -0,0 +1,12 @@
+function Ispeople (Developer,Artist,Singer,Actor) {
+    console.log(Developer + "")
+    console.log(Artist + "")
+    console.log(Singer + "")
+    console.log(Actor + "")
+}
+let Developer = "Alex is a developer"
+let Artist = "John is a Artist"
+let Singer = "Strek  is a singer"
+let Actor = "Smith is a Actor"
+
+Ispeople(Developer,Artist,Singer,Actor);
